a67bb32da738d1ef5128ca7f35c473ab4f8b5520
pha-ran <pha-ran@naver.com>
2021-03-15 02:42:30 +0900

    Socket Connect, Add Event [ message ]

--- ex_socket/server/home.js
+++ ex_socket/server/home.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var io = require('socket.io')(server);
 
 // Port setting
 var port = 2021;
@@ -11,6 +11,22 @@ server.listen(port, function(){
 // 클라이언트에서 connect에 성공 시 호출
 io.on('connection', function(socket) {
   console.log("new client connected");
+
+  // 클라이언트의 연결이 끊어졌을 때 호출
+  socket.on('disconnect', function(){
+		console.log('server disconnected');
+	});
+
+  // 오류가 난 경우 호출
+  socket.on('error', function(error){
+    console.log('error : ' + error);
+  });
+
+  // 'message'라는 이름의 event를 받았을 경우 호출
+  socket.on('message', function(obj){
+		console.log('server received data : ' + obj);
+	});
+
 });
 
 app.get('/', function(req,res) {

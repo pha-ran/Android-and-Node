f3df64da52def778828b0381dad113956f399c8a
pha-ran <pha-ran@naver.com>
2021-03-16 22:49:40 +0900

    Connect MongoDB

--- ex_socket/server/home.js
+++ ex_socket/server/home.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var mongoose = require('mongoose');
 
 // Port setting
 var port = 2021;
@@ -8,25 +9,40 @@ server.listen(port, function(){
   console.log('server on! http://localhost:'+port);
 });
 
+// DB setting (@5.11.15)
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log('DB connected');
+});
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
+});
+
 // 클라이언트에서 connect에 성공 시 호출
 io.on('connection', function(socket) {
-  console.log("new client connected");
 
-  // 클라이언트의 연결이 끊어졌을 때 호출
-  socket.on('disconnect', function(){
-		console.log('server disconnected');
-	});
+    console.log("new client connected");
 
-  // 오류가 난 경우 호출
-  socket.on('error', function(error){
-    console.log('error : ' + error);
-  });
+    // 클라이언트의 연결이 끊어졌을 때 호출
+    socket.on('disconnect', function(){
+		  console.log('server disconnected');
+	  });
 
-  // 'message'라는 이름의 event를 받았을 경우 호출
-  socket.on('message', function(obj){
-		console.log('server received data : ' + obj);
-    socket.emit('message', "OK");
-	});
+    // 오류가 난 경우 호출
+    socket.on('error', function(error){
+      console.log('error : ' + error);
+    });
+
+    // 'message'라는 이름의 event를 받았을 경우 호출
+    socket.on('message', function(obj){
+		  console.log('server received data : ' + obj);
+      socket.emit('message', "OK");
+	  });
 
 });
 

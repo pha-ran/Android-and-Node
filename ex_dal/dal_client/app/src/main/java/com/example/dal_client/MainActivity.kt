031b2ece8d1c057f4924478349290cc1377b8cbe
pha-ran <pha-ran@naver.com>
2021-04-21 23:30:57 +0900

    Add Splash

--- ex_dal/dal_client/app/src/main/java/com/example/dal_client/MainActivity.kt
+++ ex_dal/dal_client/app/src/main/java/com/example/dal_client/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_MaterialComponents_Light_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
